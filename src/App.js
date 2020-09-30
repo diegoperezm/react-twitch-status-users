546d908c60b60e77b1983ceb62249ba0ba0d1355
erretres <diegoperezcorreo@gmail.com>
2020-09-30 13:32:48 -0300

    Refactor: Move constants to utils.
    The const:
    - USERS
    - PATH_BASE
    - PARAM_USER
    - PARAM_STREAM
    - USER_URL_ARR
    - STREAM_URL_ARR
    
    Only import (App.js):
    
    - USER_URL_ARR
    - STREAM_URL_ARR

--- src/App.js
+++ src/App.js
@@ -2,25 +2,9 @@ import  React  from 'react';
 import  { useState, useEffect }  from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
 import  Menu from './components/Menu';
 import  Table from './components/Table';
-
- const USERS = [
-  "ESL_SC2",
-  "OgamingSC2",
-  "cretetion",
-  "freecodecamp",
-  "storbeck",
-  "habathcx",
-  "RobotCaleb",
-  "noobs2ninjas"];
-
- const PATH_BASE  = 'https://wind-bow.glitch.me/twitch-api';
- const PARAM_USER = '/users/';
- const PARAM_STREAM =  '/streams/';
- const USER_URL_ARR = USERS.map(ele => `${PATH_BASE}${PARAM_USER}${ele}`);
- const STREAM_URL_ARR = USERS.map(ele => `${PATH_BASE}${PARAM_STREAM}${ele}`);
+import { USER_URL_ARR, STREAM_URL_ARR }  from './utils/';
 
 function App() {
   const [isLoading, setLoading]  = useState(true);
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,22 @@
+ const USERS = [
+  "ESL_SC2",
+  "OgamingSC2",
+  "cretetion",
+  "freecodecamp",
+  "storbeck",
+  "habathcx",
+  "RobotCaleb",
+  "noobs2ninjas"];
+
+ const PATH_BASE  = 'https://wind-bow.glitch.me/twitch-api';
+ const PARAM_USER = '/users/';
+ const PARAM_STREAM =  '/streams/';
+
+ const USER_URL_ARR = USERS.map(ele => `${PATH_BASE}${PARAM_USER}${ele}`);
+ const STREAM_URL_ARR = USERS.map(ele => `${PATH_BASE}${PARAM_STREAM}${ele}`);
+
+
+
 const placeholder = "./img/placeholder.png";
 
 function formatUsersData (list, status) {
@@ -34,3 +53,5 @@ function formatUsersData (list, status) {
 
 
 exports.formatUsersData = formatUsersData;
+exports.USER_URL_ARR= USER_URL_ARR;
+exports.STREAM_URL_ARR= STREAM_URL_ARR;
